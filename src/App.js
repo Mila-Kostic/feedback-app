14cedf836453cb17d7eb58c2990e323e83318bc7
Mila Kostic <milakostic.eng@gmail.com>
2022-07-23 22:55:43 +0200

    lists in JSX

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import React from "react";
 function App() {
   const title = "Blog Post";
   const body = "This is my blog post";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
   return (
     <div className="container">
-      <h1>{title}</h1>
+      <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
       {Math.random() * (5 + 5)}
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 //II way
 // function App() {
 //   return React.createElement(
-//     "div",
-//     { className: "container" },
-//     React.createElement("h1", {}, "My App")
+//     'div',
+//     { className: 'container'},
+//     React.createElement('h1', {}, 'My App')
 //   );
 // }
 export default App;
